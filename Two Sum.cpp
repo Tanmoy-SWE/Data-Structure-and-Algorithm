b020041dc1d6956479ad8897211706121875ac30
Tanmoy-SWE <79531921+Tanmoy-SWE@users.noreply.github.com>
2022-01-26 00:00:41 +0600

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 using namespace std;
 
-vector <int, int> getSum(int arr[], int size, int n){
+void getSum(int arr[], int size, int n){
     for(int i=0; i<size; i++){
         int start = i;
-        int end = size - 1;
+        int end = size;
         int rem = n - arr[i];
         while(start<end){
             int mid = (start + end)/2;
@@ -16,11 +16,12 @@ vector <int, int> getSum(int arr[], int size, int n){
                 end = mid - 1;
             }
             else{
-                return {i, mid};
+                cout << arr[i] << " " << arr[mid];
+                return;
             }
         }
     }
-    return {-1, -1};
+    cout << "No valid sum" << endl;
 }
 
 
@@ -31,14 +32,12 @@ int main() {
     cout << "Enter size of the array : " << endl;
     cin >> size;
     cout << "Enter the array elements : " << endl;
-    for(int i=0; i<15; i++){
+    for(int i=0; i<size; i++){
         cin >> arr[i];
     }
     int seek;
     cout << "Enter the sum you are seeking : " << endl;
     cin >> seek;
-    vector <int, int> ans;
-    ans = getSum(arr,size,seek);
+    getSum(arr,size,seek);
     return 0;
 }
-
