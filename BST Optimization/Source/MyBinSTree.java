145609679e841488fde48afd5ef6758920b3e292
Tanmoy-SWE <79531921+Tanmoy-SWE@users.noreply.github.com>
2023-04-15 21:23:51 +0600

    Rebalancing Required, Basic functionality works

--- BST Optimization/Source/MyBinSTree.java
+++ BST Optimization/Source/MyBinSTree.java
@@ -3,52 +3,77 @@ package binpacking;
 import static binpacking.Internals.*;
 
 public class MyBinSTree implements
-  BinSTreeIntf,
-  WithNodeType<MyBinSTreeNode>,
-  WithRebalanceFunction {
+        BinSTreeIntf,
+        WithNodeType<MyBinSTreeNode>,
+        WithRebalanceFunction {
 
   /**
    * <b>Don't delete this function.</b> The tests will use it.
    */
+  private MyBinSTreeNode root;
+  private int size;
+
   public MyBinSTree() {
-    // TODO: implement this!
-    //    if node == NULL
-//    return createNode(data)
-//    if (data < node->data)
-//      node->left  = insert(node->left, data);
-//    else if (data > node->data)
-//      node->right = insert(node->right, data);
+    this.root = null;
+    this.size = 0;
   }
 
   public int size() {
-    // TODO: implement this!
-    return 0;
-  }
-  public int height() {
-    // TODO: implement this!
-    return 0;
+    return this.size;
   }
 
+  public int height() {
+    if (this.root == null) {
+      return 0;
+    } else {
+      return this.root.getheight();
+    }
+  }
+
+
   public MyBinSTreeNode getRootNode() {
-    // TODO: implement this!
-    return null;
+    return this.root;
   }
 
   public void addNode(BinData binData) {
-    // TODO: implement this!
+    MyBinSTreeNode newNode = new MyBinSTreeNode(binData);
+    if (this.root == null) {
+      this.root = newNode;
+    } else {
+      this.root = this.root.addNode(newNode);
+    }
+    this.size++;
   }
 
   public BinData getSmallestBinWithMinCapacity(int minCapacity) {
-    // TODO: implement this!
-    return null;
+    if (this.root == null) {
+      return null;
+    } else {
+      MyBinSTreeNode node = this.root.findSmallestBinWithMinCapacity(minCapacity);
+      if (node == null) {
+        return null;
+      } else {
+        return node.getBinData();
+      }
+    }
   }
 
   public BinData popSmallestBinWithMinCapacity(int minCapacity) {
-    // TODO: implement this!
-    return null;
+    if (this.root == null) {
+      return null;
+    } else {
+      MyBinSTreeNode[] result = this.root.popSmallestBinWithMinCapacity(minCapacity);
+      this.root = result[0];
+      this.size--;
+      return result[1] != null ? result[1].getBinData() : null;
+    }
   }
 
   public void rebalance() {
-    // TODO: implement this!
+    if (this.root == null) {
+      return;
+    } else {
+      this.root = this.root.rebalance();
+    }
   }
 }
