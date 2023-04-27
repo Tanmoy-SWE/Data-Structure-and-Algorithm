a0d3f06c9ec2c709624f1c3d761bc4452ad777a5
Tanmoy-SWE <79531921+Tanmoy-SWE@users.noreply.github.com>
2023-04-27 23:05:13 +0600

    Works as each node in BST. Requires testing in Rebalancing

--- BST Optimization/Source/MyBinSTreeNode.java
+++ BST Optimization/Source/MyBinSTreeNode.java
@@ -9,63 +9,72 @@ import static binpacking.Internals.*;
  * can call getLeftChild() and getLeftChild() safely.
  */
 public class MyBinSTreeNode implements IsABinSTreeNode<MyBinSTreeNode> {
+
     private BinData binData;
     private MyBinSTreeNode leftChild;
     private MyBinSTreeNode rightChild;
 
-  /**
-   * INVARIANT:
-   * <li>this node's getBinData().capacity is at least as big as (&ge;)
-   *      the capacity of all children and descendants on the left.</li>
-   * <li>this node's getBinData().capacity is no bigger than (&le;)
-   *      the capacity of all children and descendants on the right.</li>
-   */
-
-  public MyBinSTreeNode(BinData binData) {
-      this.binData = binData;
-  }
-  public BinData getBinData() {
-    // TODO: implement this!
-    return binData;
-  }
-
-  public void setLeftChild(MyBinSTreeNode leftChild) {
-        this.leftChild = leftChild;
-  }
-  // returns null if no such child
-  public MyBinSTreeNode getLeftChild() {
-    // TODO: implement this!
-    return leftChild;
-  }
-
-  public void setRightChild(MyBinSTreeNode rightChild) {
-        this.rightChild = rightChild;
-  }
-  // returns null if no such child
-  public MyBinSTreeNode getRightChild() {
-    // TODO: implement this!
-    return rightChild;
-  }
-
-  //function to calculate larger number
-    public int max(int a, int b){
-      if(a>b){
-          return a;
-      }
-      return b;
+    /**
+     * INVARIANT:
+     * <li>this node's getBinData().capacity is at least as big as (&ge;)
+     *      the capacity of all children and descendants on the left.</li>
+     * <li>this node's getBinData().capacity is no bigger than (&le;)
+     *      the capacity of all children and descendants on the right.</li>
+     */
+    public MyBinSTreeNode(BinData binData) {
+        this.binData = binData;
     }
-  public int height(MyBinSTreeNode node) {
-      if (node == null) {
-          return 0;
-      } else {
-          int leftHeight = height(node.getLeftChild());
-          int rightHeight = height(node.getRightChild());
-          return Math.max(leftHeight, rightHeight) + 1;
-      }
-  }
 
-    // fuction to calculate the height/ level of tree
-    public int getheight(){
-      return height(this);
+    public BinData getBinData() {
+        return binData;
+    }
+    public void setBinData(BinData binData) {
+        this.binData = binData;
+    }
+
+    public MyBinSTreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public MyBinSTreeNode getRightChild() {
+        return rightChild;
+    }
+
+    /**
+     * Set the left child of this node, and update the invariant.
+     */
+    public void setLeftChild(MyBinSTreeNode leftChild) {
+        this.leftChild = leftChild;
+        updateInvariant();
+    }
+
+    /**
+     * Set the right child of this node, and update the invariant.
+     */
+    public void setRightChild(MyBinSTreeNode rightChild) {
+        this.rightChild = rightChild;
+        updateInvariant();
+    }
+
+    /**
+     * Update the invariant for this node, and recursively for all its
+     * descendants.
+     */
+    private void updateInvariant() {
+        // Check the invariant for the left child.
+        if (leftChild != null && binData.capacity < leftChild.binData.capacity) {
+            throw new RuntimeException("Invariant violation: left child has greater capacity");
+        }
+        // Check the invariant for the right child.
+        if (rightChild != null && binData.capacity > rightChild.binData.capacity) {
+            throw new RuntimeException("Invariant violation: right child has lesser capacity");
+        }
+        // Recursively update the invariant for all descendants.
+        if (leftChild != null) {
+            leftChild.updateInvariant();
+        }
+        if (rightChild != null) {
+            rightChild.updateInvariant();
+        }
     }
 }
